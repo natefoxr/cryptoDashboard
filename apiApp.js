104c1bc2c20b1bc112f5df6bb278fed8f774f76c
natefoxr <nate@foxr.xyz>
2022-07-11 01:56:42 -0700

    Added a trending section

--- apiApp.js
+++ apiApp.js
@@ -12,14 +12,12 @@ function coinGeckoSearch(userinput){
                 const mcRank = element['market_cap_rank']
                 const symbol = element['symbol']
                 const thumbnail = element['large']
-
-
                 $(`.coindata`).append(
                     `     
-                    <div class="col-md-6 col-xl-4 justify-content-center">
+                    <div class="col-md-6 col-xl-3 justify-content-center">
                         <div class="card w-md-75 mx-auto my-4 text-center">
                             <div class="img">
-                                <img class="card-img-top w-50 h-50 rounded" src="${thumbnail}" alt="Crypto image">
+                                <img class="card-img-top w-50 h-50 rounded" src="${thumbnail}" alt="Crypto image"">
                             </div>
                             <div class="card-body">
                                 <h1 class="card-title capitalize pb-3">${name}</h1>
@@ -33,10 +31,8 @@ function coinGeckoSearch(userinput){
             })
             $('.crypto-details').on('click', function() {
                 let cryptoId = $(this).text().slice(0,-8)
-                console.log(cryptoId)
                 $.get(
                     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${cryptoId}&order=market_cap_desc&per_page=100&page=1&sparkline=false`, function(data) {
-                        console.log(data)
                         const response = data[0]
                         const detailName = response['name'];
                         const detailSymbol = response['symbol'];
@@ -78,6 +74,86 @@ function coinGeckoSearch(userinput){
     })
 }
 
+function coinGeckoTrending() {
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+    $.get(
+        `https://api.coingecko.com/api/v3/search/trending`, function(data) {
+            $(`.coindata`).html("")
+            console.log(data)
+            data['coins'].forEach(element => {
+                const item = element['item']
+                const id = item['id']
+                const name = item['name']
+                const mcRank = item['market_cap_rank']
+                const symbol = item['symbol']
+                const thumbnail = item['large']
+                $(`.coindata`).append(
+                    `     
+                    <div class="col-md-6 col-xl-3 justify-content-center">
+                        <div class="card w-md-75 mx-auto my-4 text-center">
+                            <div class="img">
+                                <img class="card-img-top w-50 h-50 rounded" src="${thumbnail}" alt="Crypto image">
+                            </div>
+                            <div class="card-body">
+                                <h1 class="card-title capitalize pb-3">${name}</h1>
+                                <p class="card-text">${symbol} Market Cap Rank: <strong>${mcRank}</strong></p>
+                                <a class="btn btn-success capitalize crypto-details">${id} details</a>
+                            </div>
+                        </div>
+                    </div>
+                    `
+                )
+            })
+            $('.crypto-details').on('click', function() {
+                let cryptoId = $(this).text().slice(0,-8)
+                console.log(cryptoId)
+                $.get(
+                    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${cryptoId}&order=market_cap_desc&per_page=100&page=1&sparkline=false`, function(data) {
+                        console.log(data)
+                        const response = data[0]
+                        const trendingName = response['name'];
+                        const trendingSymbol = response['symbol'];
+                        const trendingPrice = response['current_price'];
+                        const trendingMc = response['market_cap'];
+                        const trendingVol = response['total_volume'];
+                        const trendingChange24 = response['price_change_percentage_24h'];
+                        const trendingLow24 = response['low_24h'];
+                        const trendingHigh24 = response['high_24h'];
+                        const trendingSupply = response['total_supply'];
+                        const trendingAth = response['ath'];
+                        const trendingCryptoImage = response['image'] 
+                        $(`.coindata`).html(
+                            `
+                            <div class="container my-4 mx-auto text-center text-light">
+                                <div class="img">
+                                    <img class="card-img-top w-25 h-25 rounded" src="${trendingCryptoImage}" alt="Crypto image">
+                                </div>
+                                <div class="card-body">
+                                    <h1 class="card-title capitalize pb-3">${trendingName}</h1>
+                                    <p class="card-text">${trendingSymbol.toUpperCase()}-USD Price: <strong>${formatter.format(trendingPrice)}</strong></p>
+                                    <p class="card-text">${trendingSymbol.toUpperCase()}-USD Market Cap: <strong>${formatter.format(trendingMc)}</strong></p>
+                                    <p class="card-text">${trendingSymbol.toUpperCase()}-USD Volume: <strong>${formatter.format(trendingVol)}</strong></p>
+                                    <p class="card-text">${trendingSymbol.toUpperCase()}-USD 24HR Change: <strong class="percentChange">${trendingChange24}%</strong></p>
+                                    <p class="card-text">${trendingSymbol.toUpperCase()}-USD 24HR Low: <strong>${formatter.format(trendingLow24)}</strong></p>
+                                    <p class="card-text">${trendingSymbol.toUpperCase()}-USD 24HR High: <strong>${formatter.format(trendingHigh24)}</strong></p>
+                                    <p class="card-text">${trendingSymbol.toUpperCase()} Supply: <strong>${trendingSupply}</strong></p>
+                                    <p class="card-text">${trendingSymbol.toUpperCase()}-USD All Time High: <strong>${formatter.format(trendingAth)}</strong></p>
+                                    <a href="./index.html" class="btn btn-success ${trendingSymbol}-details">Back</a>
+                                </div>
+                            </div>
+                            `
+                        )
+                    }
+                )
+                            
+            }
+        )
+    })
+}
+
 function coinGeckoData(coin) {
     const formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
@@ -102,7 +178,7 @@ function coinGeckoData(coin) {
                 const cryptoImage = data['image']
                 $(`.coindata`).append(
                     `     
-                    <div class="col-md-6 col-xl-4 justify-content-center">
+                    <div class="col-md-6 col-xl-3 justify-content-center">
                         <div class="card w-md-75 mx-auto my-4 text-center">
                             <div class="img">
                                 <img class="card-img-top w-50 h-50 rounded" src="${cryptoImage}" alt="Crypto image">
@@ -126,10 +202,8 @@ function coinGeckoData(coin) {
             });
             $('.crypto-details').on('click', function() {
                 let cryptoId = $(this).text().slice(0,-8)
-                console.log(cryptoId)
                 $.get(
                     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${cryptoId}&order=market_cap_desc&per_page=100&page=1&sparkline=false`, function(data) {
-                        console.log(data)
                         const response = data[0]
                         const detailName = response['name'];
                         const detailSymbol = response['symbol'];
@@ -179,6 +253,14 @@ $(document).ready(function() {
         <nav class="navbar bg-light">
             <div class="container-fluid">
                 <a href="./index.html" class="navbar-brand">FOXR DASHBOARD</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <button href="./index.html" class="nav-link border-0 bg-light trending">Trending Coins</button>
+                    </li>
                 <form class="d-flex" role="search">
                     <input class="form-control me-2 search-value" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success search-button">Search</button>
@@ -193,10 +275,12 @@ $(document).ready(function() {
         coinGeckoSearch($('.search-value').val())
     })
 
+    $('.trending').on('click', function(e) {
+        e.preventDefault()
+        coinGeckoTrending()
+    })
+
 
     // trending section
-    $.get(
-        `https://api.coingecko.com/api/v3/search/trending`, function(data) {
-            console.log(data)
-    })
+
 })
